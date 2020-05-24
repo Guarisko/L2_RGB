b61349d5a4ff9fb698ac028303883106ca77959a
Guarisko <33266620+Guarisko@users.noreply.github.com>
2020-05-24 15:46:47 +0300

    Initial Commit

---
+++ L2_RGB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  L2_RGB
+//
+//  Created by Алексей Корзин on 24.05.2020.
+//  Copyright © 2020 Алексей Корзин. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
